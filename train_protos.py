875c42881b3d25185c5e0506f8708bb14598c135
Pablo <pablo.alonso@upf.edu>
2023-12-22 02:03:04 +0100

    Add scheduler

--- train_protos.py
+++ train_protos.py
@@ -34,38 +34,47 @@ def dataset_generator(metadata_file: Path, data_dir: Path):
 class ZinemaNet(L.LightningModule):
     def __init__(
         self,
+        protos: np.array,
         time_dim: int = 300,
         feat_dim: int = 768,
         n_labels: int = 10,
-        hidden_size: int = 128,
-        protos: int = 1,
         batch_size: int = 32,
+        total_steps: int = 1000,
+        weight_decay: float = 1e-4,
+        max_lr: float = 1e-3,
+        temp: float = 0.1,
+        alpha: float = 0.5,
     ):
         super().__init__()
 
         self.time_dim = time_dim
         self.feat_dim = feat_dim
         self.n_labels = n_labels
-        self.hidden_size = hidden_size
-        self.protos = protos
-        self.n_protos = self.protos.shape[0]
-        self.n_protos_per_class = self.n_protos // self.n_labels
+        self.protos_weights = protos
+        self.batch_size = batch_size
+        self.total_steps = total_steps
+        self.weight_decay = weight_decay
+        self.max_lr = max_lr
+        self.temp = temp
+        self.alpha = alpha
 
-        self.protos = nn.Parameter(data=torch.tensor(self.protos), requires_grad=True)
+        self.n_protos = self.protos.shape[0]
+        self.n_protos_per_label = self.n_protos // self.n_labels
+
+        self.protos = nn.Parameter(
+            data=torch.tensor(self.protos_weights), requires_grad=True
+        )
         self.linear = torch.nn.Linear(self.n_protos, self.n_labels)
 
         # init linear weights to direct connection to the class
         lin_weights = np.hstack(
-            [[i] * self.n_protos_per_class for i in range(self.n_labels)]
+            [[i] * self.n_protos_per_label for i in range(self.n_labels)]
         )
         lin_weights = torch.nn.functional.one_hot(torch.tensor(lin_weights))
-        print(lin_weights.T)
         self.linear.weight = nn.Parameter(
-            data=lin_weights.float().T, requires_grad=True
+            data=lin_weights.T.float(), requires_grad=True
         )
 
-        # self.l2 = nn.MSELoss()
-        #  self.bilinear = BilinearSimilarity(n_features=self.feat_dim, device="cuda:0")
         self.info_nce = InfoNCE(negative_mode=None)
         self.xent = nn.CrossEntropyLoss()
 
@@ -74,56 +83,48 @@ class ZinemaNet(L.LightningModule):
             num_classes=n_labels,
         )
 
+        # to use the scheduler
+        self.automatic_optimization = False
+
     def training_step(self, batch, batch_idx, split="train"):
-        # training_step defines the train loop.
-        # it is independent of forward
         x = batch["feature"]
         y = batch["label"]
 
-        # x = x.unsqueeze(1).repeat(1, self.n_protos, 1, 1)
-        # print("x_shape", x.shape)
-        # protos_batch = (
-        #     self.protos.unsqueeze(0).repeat(x.shape[0], 1, 1, 1).to(self.device)
-        # )
-        # print("protos_batch_shape", protos_batch.shape)
-
-        # distance = torch.cdist(x, protos_batch)
-        # print("distance_shape", distance.shape)
-
-        # todo: improve
-        # distance_reduce = distance.mean(dim=[2, 3])
-        # print("distance_reduce_shape", distance_reduce.shape)
-
-        # loss_p = torch.mean(torch.min(distance_reduce, dim=1).values) + torch.mean(
-        #     torch.min(distance_reduce, dim=0).values
-        # )
-        # x = torch.mean(x, dim=[1])
-        # protos = torch.mean(self.protos, dim=[1])
+        # flatten time and embedding dimension
         x = x.flatten(1, 2)
         protos = self.protos.flatten(1, 2)
 
-        # distance = self.bilinear(x, protos)
+        if split == "train":
+            optimizer = self.optimizers()
+            optimizer.zero_grad()
+
         similarity = self.info_nce(x, protos, output="logits")
+
         distance = torch.exp(-similarity)
         self.log(f"{split}_distance", distance.mean())
 
         y_hat = self.linear(similarity)
-
-        temp = 0.1
-        loss_c = self.xent(y_hat / temp, y)
-
-        # minimice distance from each proto to the closest sample
-        loss_p = torch.mean(torch.min(distance, dim=0).values)
-        alpha = 0.7
-        loss = alpha * loss_p + (1 - alpha) * loss_c
-
-        self.log(f"{split}_loss", loss, prog_bar=True)
-        self.log(f"{split}_proto_loss", loss_p)
-        self.log(f"{split}_class_loss", loss_c)
-
         acc = self.accuracy(y_hat, y)
 
+        # classification loss
+        loss_c = self.xent(y_hat / self.temp, y)
+        # prototype loss
+        loss_p = torch.mean(torch.min(distance, dim=0).values)
+
+        loss = self.alpha * loss_p + (1 - self.alpha) * loss_c
+
+        if split == "train":
+            self.manual_backward(loss)
+            optimizer.step()
+
+            scheduler = self.lr_schedulers()
+            scheduler.step()
+            self.log(f"{split}_lr", scheduler.get_last_lr()[0])
+
+        self.log(f"{split}_loss", loss, prog_bar=True)
         self.log(f"{split}_acc", acc, prog_bar=True)
+        self.log(f"{split}_proto_loss", loss_p)
+        self.log(f"{split}_class_loss", loss_c)
 
         return loss
 
@@ -134,8 +135,16 @@ class ZinemaNet(L.LightningModule):
         self.training_step(batch, batch_idx, split="test")
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-4, weight_decay=1e-5)
-        return optimizer
+        optimizer = optim.Adam(
+            self.parameters(),
+            weight_decay=self.weight_decay,
+        )
+        scheduler = optim.lr_scheduler.OneCycleLR(
+            optimizer,
+            max_lr=self.max_lr,
+            total_steps=self.total_steps,
+        )
+        return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
 
 def trim_embeddings(examples: list, timestamps: int = 300, mode: str = "middle"):
@@ -198,6 +207,7 @@ def create_protos(
     shape: tuple,
     n_protos_per_label: int,
     labels: set = None,
+    proto_file: Path = None,
 ):
     """Init prototypes to random weights, class centroids (kmeans) or to the sample closses to the class centroid (kmeans-sample)"""
 
@@ -206,6 +216,16 @@ def create_protos(
 
     if protos_init == "random":
         protos = torch.randn([n_protos, *shape])
+
+    elif protos_init == "proto-file":
+        protos = np.load(proto_file)
+        print(f"protos loaded from {proto_file}")
+        protos = torch.tensor(protos)
+        assert protos.shape == (
+            n_protos,
+            *shape,
+        ), f"protos shape mismatch. found: {protos.shape} expected: {(n_protos, *shape)}"
+
     elif protos_init in ("kmeans-centers", "kmeans-samples"):
         if kmeans_file.exists():
             print("kmeans data file found, loading...")
@@ -257,7 +277,19 @@ def train(
     protos_init: str = None,
     n_protos_per_label: int = 1,
     batch_size: int = 32,
+    seed: int = 42,
+    epochs: int = 1000,
+    max_lr: float = 1e-3,
+    val_test_ratio: float = 0.1,
+    timestamps: int = 300,
+    trim_mode: str = "middle",
+    gpu_id: int = 0,
+    proto_file: Path = None,
+    temp: float = 0.1,
+    alpha: float = 0.5,
 ):
+    hyperparams = locals()
+
     print("creating dataset")
     ds = Dataset.from_generator(
         dataset_generator,
@@ -267,28 +299,31 @@ def train(
         },
     )
 
-    # compress rows
-    print("cleaning dataset")
-    ds = ds.map(trim_embeddings, batched=True, num_proc=32, batch_size=32)
+    # trim embeddings, select beggining, middle or random chunk
+    print("trimming embeddings")
+    ds = ds.map(
+        trim_embeddings,
+        batched=True,
+        num_proc=32,
+        batch_size=32,
+        fn_kwargs={"timestamps": timestamps, "mode": trim_mode},
+    )
 
-    # onehot encode
-    print("cleaning labels")
+    print("label encoding")
     ds = ds.map(label_to_idx, batched=True, batch_size=len(ds))
 
-    # norm
+    # TODO: check if norm is needed
     # ds = ds.map(norm, batched=True, batch_size=len(ds))
 
     # dataset to CUDA
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     ds = ds.with_format("torch", device=device)
 
-    # splits
-    print("creating train split")
-    ds = ds.train_test_split(test_size=0.1, seed=seed)
+    print("creating train splits")
+    ds = ds.train_test_split(test_size=val_test_ratio, seed=seed)
     ds_train = ds["train"]
     ds_test = ds["test"]
-    print("creating validation split")
-    ds_tran_val = ds_train.train_test_split(test_size=0.1, seed=seed)
+    ds_tran_val = ds_train.train_test_split(test_size=val_test_ratio, seed=seed)
     ds_train = ds_tran_val["train"]
     ds_val = ds_tran_val["test"]
 
@@ -307,24 +342,48 @@ def train(
         (time_dim, feat_dim),
         n_protos_per_label=n_protos_per_label,
         labels=labels,
+        proto_file=proto_file,
+    )
+
+    loader_train = utils.data.DataLoader(
+        ds_train,
+        batch_size=batch_size,
+        shuffle=True,
+        # num_workers=20,
+    )
+    loader_val = utils.data.DataLoader(
+        ds_val,
+        batch_size=batch_size,
+        # num_workers=20,
+    )
+    loader_test = utils.data.DataLoader(
+        ds_test,
+        batch_size=batch_size,
+        # num_workers=10,
     )
 
     model = ZinemaNet(
+        protos,
         time_dim=time_dim,
         feat_dim=feat_dim,
         n_labels=n_labels,
-        protos=protos,
+        batch_size=batch_size,
+        total_steps=len(loader_train) * epochs,
+        temp=temp,
+        alpha=alpha,
+        max_lr=max_lr,
     )
 
-    loader_train = utils.data.DataLoader(ds_train, batch_size=batch_size, shuffle=True)
-    loader_val = utils.data.DataLoader(ds_val, batch_size=batch_size)
-    loader_test = utils.data.DataLoader(ds_test, batch_size=batch_size)
+    logger = TensorBoardLogger(
+        "tb_logs",
+        name="zinemanet",
+    )
 
-    logger = TensorBoardLogger("tb_logs", name="zinemanet")
+    logger.log_hyperparams(hyperparams)
 
     trainer = L.Trainer(
-        max_epochs=1000,
-        devices=[0],
+        max_epochs=epochs,
+        devices=[gpu_id],
         reload_dataloaders_every_n_epochs=1,
         num_sanity_val_steps=0,
         precision="16-mixed",
@@ -336,10 +395,11 @@ def train(
         loader_val,
     )
     trainer.test(model, loader_test)
+
+    # save parameters
     lin_weights = model.linear.weight.detach().cpu().numpy()
     protos = model.protos.detach().cpu().numpy()
 
-    # Get current timestamp
     current_timestamp = datetime.now()
     formatted_timestamp = current_timestamp.strftime("%y%m%d_%H%M%S")
 
@@ -349,12 +409,30 @@ def train(
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+
     parser.add_argument("--data-dir", type=Path, required=True)
     parser.add_argument("--metadata-file", type=Path, required=True)
     parser.add_argument(
-        "--protos-init", choices=["random", "kmeans-centers", "kmeans-samples"]
+        "--protos-init",
+        choices=[
+            "random",
+            "kmeans-centers",
+            "kmeans-samples",
+            "proto-file",
+        ],
     )
     parser.add_argument("--n-protos-per-label", type=int, default=1)
+    parser.add_argument("--batch-size", type=int, default=32)
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--epochs", type=int, default=1000)
+    parser.add_argument("--max-lr", type=float, default=1e-3)
+    parser.add_argument("--proto-file", type=Path, default=None)
+    parser.add_argument("--val-test-ratio", type=float, default=0.1)
+    parser.add_argument("--timestamps", type=int, default=300)
+    parser.add_argument("--trim-mode", type=str, default="middle")
+    parser.add_argument("--gpu-id", type=int, default=0)
+    parser.add_argument("--temp", type=float, default=0.1)
+    parser.add_argument("--alpha", type=float, default=0.7)
 
     args = parser.parse_args()
     train(
@@ -362,4 +440,14 @@ if __name__ == "__main__":
         metadata_file=args.metadata_file,
         protos_init=args.protos_init,
         n_protos_per_label=args.n_protos_per_label,
+        batch_size=args.batch_size,
+        seed=args.seed,
+        epochs=args.epochs,
+        max_lr=args.max_lr,
+        proto_file=args.proto_file,
+        val_test_ratio=args.val_test_ratio,
+        timestamps=args.timestamps,
+        trim_mode=args.trim_mode,
+        temp=args.temp,
+        alpha=args.alpha,
     )

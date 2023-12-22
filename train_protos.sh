c7ee3ae1b6ad6363a369feb3fd6984b3965d67c7
Pablo <pablo.alonso@upf.edu>
2023-12-22 02:03:22 +0100

    Add launch script

---
+++ train_protos.sh
@@ -0,0 +1,5 @@
+python train_protos.py \
+    --data-dir feats_encodecmae_base \
+    --metadata-file /mnt/mtgdb-audio/stable/genre_tzanetakis/metadata/groundtruth.yaml \
+    --protos-init proto-file \
+    --proto-file /home/palonso/reps/xai-encodec/protos_231222_003500.npy \

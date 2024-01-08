48212271486557886df4823ea5ad9ce0a3e4cded
Pablo <pablo.alonso@upf.edu>
2024-01-08 13:44:28 +0100

    Support diffusion generation model

--- decode_protos.py
+++ decode_protos.py
@@ -1,6 +1,8 @@
 import functools
+from functools import partial
 from argparse import ArgumentParser
 from pathlib import Path
+from math import pi
 
 import torch
 import numpy as np
@@ -8,6 +10,7 @@ import pytorch_lightning as pl
 from encodec import EncodecModel
 from essentia.standard import MonoWriter
 from huggingface_hub import hf_hub_download
+from tqdm import tqdm
 
 from encodecmae import load_model
 from encodecmae.tasks.models.transformers import (
@@ -16,34 +19,10 @@ from encodecmae.tasks.models.transformers import (
     SinusoidalPositionalEmbeddings,
 )
 
+from labelmaps import xaigenre_id2label, gtzan_id2label, nsynth_id2label
+
 sr = 24000
 
-
-label_map = {
-    0: "blu",
-    1: "cla",
-    2: "cou",
-    3: "dis",
-    4: "hip",
-    5: "jaz",
-    6: "met",
-    7: "pop",
-    8: "reg",
-    9: "roc",
-}
-
-label_map = {
-    0: "bass",
-    1: "brass",
-    2: "flute",
-    3: "guitar",
-    4: "keyboard",
-    5: "mallet",
-    6: "organ",
-    7: "reed ",
-    8: "string",
-    9: "vocal",
-}
 
 model_map = {
     "base": 768,
@@ -52,6 +31,7 @@ model_map = {
 
 parser = ArgumentParser()
 parser.add_argument("version", type=str)
+parser.add_argument("dataset", choices=["xaigenre", "gtzan", "nsynth"])
 parser.add_argument("--model-size", type=str, default="base")
 parser.add_argument("--device", type=str, default="cuda:1")
 parser.add_argument("--output-dir", type=Path, default="out_data/decoded/")
@@ -59,11 +39,20 @@ parser.add_argument("--samples-file", action="store_true")
 args = parser.parse_args()
 
 version = args.version
+dataset = args.dataset
 model_size = args.model_size
 device = args.device
 output_dir = args.output_dir
 samples_file = args.samples_file
 
+if dataset == "xaigenre":
+    label_map = xaigenre_id2label
+elif dataset == "gtzan":
+    label_map = gtzan_id2label
+elif dataset == "nsynth":
+    label_map = nsynth_id2label
+else:
+    raise ValueError(f"Unknown dataset: {dataset}")
 
 if model_size == "base":
 
@@ -86,6 +75,16 @@ if model_size == "base":
             dec_out = self.decoder(x, padding_mask=None)
             y = self.decoder_proj(dec_out)
             return y
+
+    ecmae = load_model(model_size, device=device)
+    ecmae2ec = EnCodecMAEtoEnCodec(model_dim=model_map[model_size])
+    ckpt_file = hf_hub_download(
+        repo_id=f"lpepino/encodecmae-{model_size}", filename="ecmae2ec.pt"
+    )
+    ckpt = torch.load(ckpt_file, map_location=device)
+    ecmae2ec.load_state_dict(ckpt, strict=False)
+    ecmae2ec.to(device)
+    ecmae2ec.eval()
 
 elif model_size == "large":
 
@@ -110,20 +109,186 @@ elif model_size == "large":
             y = self.trans(y, padding_mask=padding_mask)
             return self.lin(y)
 
+    ecmae = load_model(model_size, device=device)
+    ecmae2ec = EnCodecMAEtoEnCodec(model_dim=model_map[model_size])
+    ckpt_file = hf_hub_download(
+        repo_id=f"lpepino/encodecmae-{model_size}", filename="ecmae2ec.pt"
+    )
+    ckpt = torch.load(ckpt_file, map_location=device)
+    ecmae2ec.load_state_dict(ckpt, strict=False)
+    ecmae2ec.to(device)
+    ecmae2ec.eval()
+
+
+elif model_size == "base_diffusion":
+    diffusion_steps = 35
+    guidance_strength = 1
+    win_size = 24000
+    hop_size = 24000
+
+    class TransformerCLSEncoder(torch.nn.Module):
+        def __init__(
+            self,
+            encodecmae_model="base",
+            num_heads=12,
+            num_encoder_layers=2,
+            num_cls_tokens=1,
+            device="cpu",
+            downsample_factor=75,
+        ):
+            super().__init__()
+            self.encodecmae_model = load_model(encodecmae_model, device=device)
+            self.encodecmae_model.visible_encoder.compile = False
+            model_dim = self.encodecmae_model.visible_encoder.model_dim
+            self.encoder = TransformerEncoder(
+                model_dim,
+                attention_layer=partial(
+                    MultiHeadAttention, model_dim=model_dim, num_heads=num_heads
+                ),
+                num_layers=2,
+                compile=False,
+            )
+            self.cls_tokens = torch.nn.Embedding(num_cls_tokens, model_dim)
+            self.pos_encoder = SinusoidalPositionalEmbeddings(model_dim)
+            self.num_cls_tokens = num_cls_tokens
+            self.out_channels = model_dim
+            self.downsample_factor = downsample_factor
+
+        def forward(self, x):
+            with torch.no_grad():
+                self.encodecmae_model.encode_wav(x)
+                self.encodecmae_model.mask(x, ignore_mask=True)
+                self.encodecmae_model.encode_visible(x)
+            cls_tokens = torch.tile(
+                self.cls_tokens(
+                    torch.arange(
+                        self.num_cls_tokens, device=x["visible_embeddings"].device
+                    ).unsqueeze(0)
+                ),
+                (x["visible_embeddings"].shape[0], 1, 1),
+            )
+            enc_in = torch.cat(
+                [cls_tokens, self.pos_encoder(x["visible_embeddings"])], dim=1
+            )
+            padding_mask = torch.cat(
+                [
+                    torch.zeros(
+                        (x["visible_embeddings"].shape[0], self.num_cls_tokens),
+                        device=x["visible_embeddings"].device,
+                    ),
+                    x["feature_padding_mask"],
+                ],
+                dim=1,
+            )
+            enc_out = self.encoder(enc_in, padding_mask)
+            cls = enc_out[:, : self.num_cls_tokens]
+            x["cls_token"] = cls
+            return x
+
+    class TransformerAEDiffusion(pl.LightningModule):
+        def __init__(
+            self,
+            encodecmae_model="base",
+            num_cls_tokens=5,
+            num_encoder_layers=4,
+            num_denoiser_layers=4,
+            num_heads=8,
+            signal_to_generate="visible_embeddings",
+            signal_dim=768,
+        ):
+            super().__init__()
+            self.encoder = TransformerCLSEncoder(
+                num_cls_tokens=num_cls_tokens,
+                num_encoder_layers=num_encoder_layers,
+                encodecmae_model=encodecmae_model,
+                num_heads=num_heads,
+            )
+            self.denoiser = TransformerEncoder(
+                self.encoder.out_channels,
+                attention_layer=partial(
+                    MultiHeadAttention,
+                    model_dim=self.encoder.out_channels,
+                    num_heads=num_heads,
+                ),
+                num_layers=num_denoiser_layers,
+                compile=False,
+            )
+            self.in_adapter = torch.nn.Linear(signal_dim, self.encoder.out_channels)
+            self.out_adapter = torch.nn.Linear(self.encoder.out_channels, signal_dim)
+            self.time_embedding = torch.nn.Sequential(
+                torch.nn.Linear(1, self.encoder.out_channels),
+                torch.nn.ReLU(),
+                torch.nn.Linear(self.encoder.out_channels, self.encoder.out_channels),
+            )
+            self.signal_to_generate = signal_to_generate
+            self.signal_dim = signal_dim
+
+        def encode(self, x):
+            return self.encoder(x)
+
+        def sample(self, code, steps=10, length=75, guidance_strength=0):
+            with torch.no_grad():
+                sigmas = torch.linspace(1, 0, steps + 1, device=code.device)
+                ts = self.time_embedding(sigmas[:, None, None])
+                angle = sigmas * pi / 2
+                alphas, betas = torch.cos(angle), torch.sin(angle)
+                x_noisy = torch.randn((1, length, self.signal_dim), device=code.device)
+                noise_sequence = []
+                for i in range(steps):
+                    denoiser_in = torch.cat(
+                        [
+                            code,
+                            ts[i, :, :].unsqueeze(0),
+                            self.encoder.pos_encoder(self.in_adapter(x_noisy)),
+                        ],
+                        dim=1,
+                    )
+                    v_pred = self.out_adapter(
+                        self.denoiser(denoiser_in, None)[
+                            :, self.encoder.num_cls_tokens + 1 :
+                        ]
+                    )
+                    if guidance_strength > 0:
+                        denoiser_in_unc = torch.cat(
+                            [
+                                torch.zeros_like(code),
+                                ts[i, :, :].unsqueeze(0),
+                                self.encoder.pos_encoder(self.in_adapter(x_noisy)),
+                            ],
+                            dim=1,
+                        )
+                        v_pred_unc = self.out_adapter(
+                            self.denoiser(denoiser_in_unc, None)[
+                                :, self.encoder.num_cls_tokens + 1 :
+                            ]
+                        )
+                        v_pred += guidance_strength * (v_pred - v_pred_unc)
+                    x_pred = alphas[i] * x_noisy - betas[i] * v_pred
+                    noise_pred = betas[i] * x_noisy + alphas[i] * v_pred
+                    x_noisy = alphas[i + 1] * x_pred + betas[i + 1] * noise_pred
+                    noise_sequence.append(x_noisy)
+            return noise_sequence
+
+    ckpt_file = hf_hub_download(
+        repo_id="lpepino/encodecmae-base",
+        filename="ae/diffusion_encodec_transformer_4L8Lclsx1_punc01_250ksteps.ckpt",
+    )
+
+    ecmae = TransformerAEDiffusion(
+        num_cls_tokens=1,
+        signal_to_generate="wav_features",
+        signal_dim=128,
+        num_denoiser_layers=8,
+    )
+    ecmae.load_state_dict(torch.load(ckpt_file, map_location=device)["state_dict"])
+    ecmae.to(device)
+
 else:
     raise ValueError(f"Unknown model size: {model_size}")
 
-ecmae = load_model(model_size, device=device)
-ecmae2ec = EnCodecMAEtoEnCodec(model_dim=model_map[model_size])
-ckpt_file = hf_hub_download(
-    repo_id=f"lpepino/encodecmae-{model_size}", filename="ecmae2ec.pt"
-)
-ckpt = torch.load(ckpt_file, map_location=device)
-ecmae2ec.load_state_dict(ckpt, strict=False)
+
 ec = EncodecModel.encodec_model_24khz()
-ecmae2ec.to(device)
 ec.to(device)
-ecmae2ec.eval()
 
 if samples_file:
     prefix = "samples"
@@ -132,20 +297,40 @@ else:
 
 learned_protos = np.load(f"out_data/{prefix}_{version}.npy")
 protos_per_label = learned_protos.shape[0] // len(label_map)
+print(f"protos per label: {protos_per_label}")
 
 label_i = 0
-for i in range(learned_protos.shape[0]):
+for i_proto in range(learned_protos.shape[0]):
     print(f"class {label_map[label_i]}")
-    encmae_feats = torch.tensor(learned_protos[i], device=device).unsqueeze(0)
+    encmae_feats = torch.tensor(learned_protos[i_proto], device=device).unsqueeze(1)
 
-    with torch.no_grad():
-        ec_dec_in = ecmae2ec(encmae_feats)
-        reconstruction = ec.decoder(ec_dec_in.transpose(1, 2))
+    if model_size == "base_diffusion":
+        audio = np.zeros((encmae_feats.shape[0] - 1) * hop_size + win_size)
 
-    audio = reconstruction.cpu().numpy().squeeze()
+        for i in tqdm(range(encmae_feats.shape[0])):
+            with torch.no_grad():
+                rec = ecmae.sample(
+                    encmae_feats[i].unsqueeze(0),
+                    steps=diffusion_steps,
+                    guidance_strength=guidance_strength,
+                )
+                ecmae_features = rec[-1]
+                reconstruction = ec.decoder(ecmae_features.transpose(1, 2))
+                audio[i * hop_size : i * hop_size + win_size] += (
+                    audio[i * hop_size : i * hop_size + win_size]
+                    + reconstruction[0, 0].detach().cpu().numpy()
+                )
+
+    elif model_size in ("base", "large"):
+        with torch.no_grad():
+            ec_dec_in = ecmae2ec(encmae_feats)
+            reconstruction = ec.decoder(ec_dec_in.transpose(1, 2))
+
+        audio = reconstruction.cpu().numpy().squeeze()
 
     out_path = (
-        output_dir / f"{version}_{label_map[label_i]}_n{(i+ 1) % protos_per_label}.wav"
+        output_dir
+        / f"{version}_{label_map[label_i]}_n{(i_proto + 1) % protos_per_label}.wav"
     )
 
     if not out_path.parent.exists():
@@ -153,5 +338,5 @@ for i in range(learned_protos.shape[0]):
 
     MonoWriter(filename=str(out_path), sampleRate=sr)(audio)
 
-    if (i + 1) % protos_per_label == 0:
+    if (i_proto + 1) % protos_per_label == 0:
         label_i += 1
--- encode_mae.py
+++ encode_mae.py
@@ -1,29 +1,207 @@
 from argparse import ArgumentParser
+from functools import partial
 from pathlib import Path
+from math import pi
 from glob import glob
 
 import librosa
 import numpy as np
 import torch
-from tqdm import tqdm
 from encodec import EncodecModel
+from huggingface_hub import hf_hub_download
+from tqdm import tqdm
+import pytorch_lightning as pl
 
 from encodecmae import load_model
+from encodecmae.tasks.models.transformers import (
+    TransformerEncoder,
+    MultiHeadAttention,
+    SinusoidalPositionalEmbeddings,
+)
 
 
 parser = ArgumentParser()
 parser.add_argument("audio_dir", type=Path)
 parser.add_argument("embeddings_dir", type=Path)
-parser.add_argument("--model-size", type=str, default="base")
+parser.add_argument(
+    "--model-size",
+    type=str,
+    default="base",
+    choices=["base", "base_diffusion", "large"],
+)
 parser.add_argument("--format", type=str, default="wav", choices=["wav", "mp3"])
 parser.add_argument("--device", type=str, default="cuda:0")
 
 args = parser.parse_args()
 
 device = args.device
+model_size = args.model_size
 
+if model_size == "base_diffusion":
+    diffusion_steps = 35
+    guidance_strength = 1
+    win_size = 24000
+    hop_size = 24000
 
-ecmae = load_model("base", device=device)
+    class TransformerCLSEncoder(torch.nn.Module):
+        def __init__(
+            self,
+            encodecmae_model="base",
+            num_heads=12,
+            num_encoder_layers=2,
+            num_cls_tokens=1,
+            device="cpu",
+            downsample_factor=75,
+        ):
+            super().__init__()
+            self.encodecmae_model = load_model(encodecmae_model, device=device)
+            self.encodecmae_model.visible_encoder.compile = False
+            model_dim = self.encodecmae_model.visible_encoder.model_dim
+            self.encoder = TransformerEncoder(
+                model_dim,
+                attention_layer=partial(
+                    MultiHeadAttention, model_dim=model_dim, num_heads=num_heads
+                ),
+                num_layers=2,
+                compile=False,
+            )
+            self.cls_tokens = torch.nn.Embedding(num_cls_tokens, model_dim)
+            self.pos_encoder = SinusoidalPositionalEmbeddings(model_dim)
+            self.num_cls_tokens = num_cls_tokens
+            self.out_channels = model_dim
+            self.downsample_factor = downsample_factor
+
+        def forward(self, x):
+            with torch.no_grad():
+                self.encodecmae_model.encode_wav(x)
+                self.encodecmae_model.mask(x, ignore_mask=True)
+                self.encodecmae_model.encode_visible(x)
+            cls_tokens = torch.tile(
+                self.cls_tokens(
+                    torch.arange(
+                        self.num_cls_tokens, device=x["visible_embeddings"].device
+                    ).unsqueeze(0)
+                ),
+                (x["visible_embeddings"].shape[0], 1, 1),
+            )
+            enc_in = torch.cat(
+                [cls_tokens, self.pos_encoder(x["visible_embeddings"])], dim=1
+            )
+            padding_mask = torch.cat(
+                [
+                    torch.zeros(
+                        (x["visible_embeddings"].shape[0], self.num_cls_tokens),
+                        device=x["visible_embeddings"].device,
+                    ),
+                    x["feature_padding_mask"],
+                ],
+                dim=1,
+            )
+            enc_out = self.encoder(enc_in, padding_mask)
+            cls = enc_out[:, : self.num_cls_tokens]
+            x["cls_token"] = cls
+            return x
+
+    class TransformerAEDiffusion(pl.LightningModule):
+        def __init__(
+            self,
+            encodecmae_model="base",
+            num_cls_tokens=5,
+            num_encoder_layers=4,
+            num_denoiser_layers=4,
+            num_heads=8,
+            signal_to_generate="visible_embeddings",
+            signal_dim=768,
+        ):
+            super().__init__()
+            self.encoder = TransformerCLSEncoder(
+                num_cls_tokens=num_cls_tokens,
+                num_encoder_layers=num_encoder_layers,
+                encodecmae_model=encodecmae_model,
+                num_heads=num_heads,
+            )
+            self.denoiser = TransformerEncoder(
+                self.encoder.out_channels,
+                attention_layer=partial(
+                    MultiHeadAttention,
+                    model_dim=self.encoder.out_channels,
+                    num_heads=num_heads,
+                ),
+                num_layers=num_denoiser_layers,
+                compile=False,
+            )
+            self.in_adapter = torch.nn.Linear(signal_dim, self.encoder.out_channels)
+            self.out_adapter = torch.nn.Linear(self.encoder.out_channels, signal_dim)
+            self.time_embedding = torch.nn.Sequential(
+                torch.nn.Linear(1, self.encoder.out_channels),
+                torch.nn.ReLU(),
+                torch.nn.Linear(self.encoder.out_channels, self.encoder.out_channels),
+            )
+            self.signal_to_generate = signal_to_generate
+            self.signal_dim = signal_dim
+
+        def encode(self, x):
+            return self.encoder(x)
+
+        def sample(self, code, steps=10, length=75, guidance_strength=0):
+            with torch.no_grad():
+                sigmas = torch.linspace(1, 0, steps + 1, device=code.device)
+                ts = self.time_embedding(sigmas[:, None, None])
+                angle = sigmas * pi / 2
+                alphas, betas = torch.cos(angle), torch.sin(angle)
+                x_noisy = torch.randn((1, length, self.signal_dim), device=code.device)
+                noise_sequence = []
+                for i in range(steps):
+                    denoiser_in = torch.cat(
+                        [
+                            code,
+                            ts[i, :, :].unsqueeze(0),
+                            self.encoder.pos_encoder(self.in_adapter(x_noisy)),
+                        ],
+                        dim=1,
+                    )
+                    v_pred = self.out_adapter(
+                        self.denoiser(denoiser_in, None)[
+                            :, self.encoder.num_cls_tokens + 1 :
+                        ]
+                    )
+                    if guidance_strength > 0:
+                        denoiser_in_unc = torch.cat(
+                            [
+                                torch.zeros_like(code),
+                                ts[i, :, :].unsqueeze(0),
+                                self.encoder.pos_encoder(self.in_adapter(x_noisy)),
+                            ],
+                            dim=1,
+                        )
+                        v_pred_unc = self.out_adapter(
+                            self.denoiser(denoiser_in_unc, None)[
+                                :, self.encoder.num_cls_tokens + 1 :
+                            ]
+                        )
+                        v_pred += guidance_strength * (v_pred - v_pred_unc)
+                    x_pred = alphas[i] * x_noisy - betas[i] * v_pred
+                    noise_pred = betas[i] * x_noisy + alphas[i] * v_pred
+                    x_noisy = alphas[i + 1] * x_pred + betas[i + 1] * noise_pred
+                    noise_sequence.append(x_noisy)
+            return noise_sequence
+
+    ckpt_file = hf_hub_download(
+        repo_id="lpepino/encodecmae-base",
+        filename="ae/diffusion_encodec_transformer_4L8Lclsx1_punc01_250ksteps.ckpt",
+    )
+
+    ecmae = TransformerAEDiffusion(
+        num_cls_tokens=1,
+        signal_to_generate="wav_features",
+        signal_dim=128,
+        num_denoiser_layers=8,
+    )
+    ecmae.load_state_dict(torch.load(ckpt_file, map_location=device)["state_dict"])
+    ecmae.to(device)
+
+else:
+    ecmae = load_model("base", device=device)
 
 # ec = EncodecModel.encodec_model_24khz()
 # ec.to(device)
@@ -36,33 +214,55 @@ for audio_file in tqdm(audio_files):
     audio_file = Path(audio_file)
 
     output_path = (
-        args.embeddings_dir / args.model_size / audio_file.parent.name / audio_file.stem
+        args.embeddings_dir / args.model_size / audio_file.parent.name / audio_file.name
     ).with_suffix(".npy")
 
     if output_path.exists():
         continue
 
     xorig, fs = librosa.core.load(audio_file, sr=24000)
-    n_patches = xorig.shape[0] // (4 * fs)
-    xorig = xorig[: n_patches * 4 * fs].reshape(-1, 4 * fs)
 
-    x = {
-        "wav": torch.from_numpy(xorig).to(device=device, dtype=ecmae.dtype),
-        "wav_lens": torch.tensor(
-            [
-                xorig.shape[1],
-            ]
-            * xorig.shape[0],
-            device=device,
-        ),
-    }
-    ecmae.visible_encoder.compile = False
+    if model_size == "base_diffusion":
+        ecmae_feature_stack = []
+        for i in tqdm(range(0, len(xorig) - win_size, hop_size)):
+            x = {
+                "wav": torch.from_numpy(xorig[i : i + win_size])
+                .unsqueeze(0)
+                .to(device),
+                "wav_lens": torch.tensor(
+                    [
+                        win_size,
+                    ],
+                    device=device,
+                ),
+            }
+            with torch.no_grad():
+                ecmae.encode(x)
+                ecmae_feature_stack.append(x["cls_token"])
+        ecmae_features = torch.cat(ecmae_feature_stack, dim=0)
 
-    with torch.no_grad():
-        ecmae.encode_wav(x)
-        ecmae.mask(x, ignore_mask=True)
-        ecmae.encode_visible(x)
-        ecmae_features = x["visible_embeddings"]
+    else:
+        ecmae.visible_encoder.compile = False
+
+        n_patches = xorig.shape[0] // (4 * fs)
+        xorig = xorig[: n_patches * 4 * fs].reshape(-1, 4 * fs)
+
+        x = {
+            "wav": torch.from_numpy(xorig).to(device=device, dtype=ecmae.dtype),
+            "wav_lens": torch.tensor(
+                [
+                    xorig.shape[1],
+                ]
+                * xorig.shape[0],
+                device=device,
+            ),
+        }
+
+        with torch.no_grad():
+            ecmae.encode_wav(x)
+            ecmae.mask(x, ignore_mask=True)
+            ecmae.encode_visible(x)
+            ecmae_features = x["visible_embeddings"]
 
     output_path.parent.mkdir(parents=True, exist_ok=True)
     np.save(output_path, ecmae_features.detach().cpu().numpy())

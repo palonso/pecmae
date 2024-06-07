a3e9b01d5c3457de05f192017652e82d10c7992a
palonso <pablo.alonso@upf.edu>
2024-06-07 19:39:11 +0200

    Fix encoding script

--- src/encode_encodecmae.py
+++ src/encode_encodecmae.py
@@ -11,101 +11,113 @@ from encodecmae_to_wav.hub import load_model
 import encodecmae
 
 
-parser = ArgumentParser()
-parser.add_argument("audio_dir", type=Path)
-parser.add_argument("embeddings_dir", type=Path)
-parser.add_argument(
-    "--model",
-    type=str,
-    default="base",
-    choices=[
-        "base",
-        "large",
-        "base_diffusion",
-        "large_diffusion",
-        "large_diffusion_10s",
-    ],
-)
-parser.add_argument("--format", type=str, default="wav", choices=["wav", "mp3"])
-parser.add_argument("--device", type=str, default="cuda:0")
+sr = 24000
 
-args = parser.parse_args()
+def encode_encodecmae(
+    audio_dir: Path,
+    features_dir: Path,
+    model: str,
+    device="cuda:0",
+    audio_format=".wav",
+):
 
-device = args.device
-model = args.model
-
-if model in ("base_diffusion", "large_diffusion", "large_diffusion_10s"):
-    if model == "base_diffusion":
-        model_filename = "ecmae2ec-base-1LTransformer"
-
-    elif model == "large_diffusion":
-        model_filename = "DiffTransformerAE2L8L1CLS-4s"
-
-    elif model == "large_diffusion_10s":
-        model_filename = "DiffTransformerAE2L8L1CLS-10s"
-
-    ecmae = load_model(model_filename, device=device)
-
-else:
-    ecmae = encodecmae.load_model(model, device=device)
-
-glob_pattern = str(args.audio_dir / "**" / f"*.{args.format}")
-print(glob_pattern)
-audio_files = glob(glob_pattern, recursive=True)
-
-for audio_file in tqdm(audio_files):
-    audio_file = Path(audio_file)
-
-    output_path = (
-        args.embeddings_dir / args.model_size / audio_file.parent.name / audio_file.name
-    ).with_suffix(".npy")
-
-    if output_path.exists():
-        continue
-
-    xorig, fs = librosa.core.load(audio_file, sr=24000)
-
-    if model in ("base_diffusion", "large_diffusion", "large_diffusion_10s"):
-        ecmae_feature_stack = []
-        for i in range(0, max(len(xorig) - win_size, 1), hop_size):
-            x = {
-                "wav": torch.from_numpy(xorig[i : i + win_size])
-                .unsqueeze(0)
-                .to(device),
-                "wav_lens": torch.tensor(
-                    [
-                        win_size,
-                    ],
-                    device=device,
-                ),
-            }
-            with torch.no_grad():
-                ecmae.encode(x)
-                ecmae_feature_stack.append(x["cls_token"])
-        ecmae_features = torch.cat(ecmae_feature_stack, dim=0)
-
+    # load the model
+    if model in ("base", "large"):
+        encoder = encodecmae.load_model(model, device=device)
     else:
-        ecmae.visible_encoder.compile = False
 
-        n_patches = xorig.shape[0] // (4 * fs)
-        xorig = xorig[: n_patches * 4 * fs].reshape(-1, 4 * fs)
+        if model == "diffusion_1s":
+            model_filename = "ecmae2ec-base-1LTransformer"
+        elif model == "diffusion_4s":
+            model_filename = "DiffTransformerAE2L8L1CLS-4s"
+        elif model == "diffusion_10s":
+            model_filename = "DiffTransformerAE2L8L1CLS-10s"
+        else:
+            raise ValueError(f"{model} is not implemented")
 
-        x = {
-            "wav": torch.from_numpy(xorig).to(device=device, dtype=ecmae.dtype),
-            "wav_lens": torch.tensor(
-                [
-                    xorig.shape[1],
-                ]
-                * xorig.shape[0],
-                device=device,
-            ),
-        }
+        encoder = load_model(model_filename, device=device)
 
-        with torch.no_grad():
-            ecmae.encode_wav(x)
-            ecmae.mask(x, ignore_mask=True)
-            ecmae.encode_visible(x)
-            ecmae_features = x["visible_embeddings"]
+    # find audio files
+    glob_pattern = str(audio_dir / "**" / f"*{audio_format}")
+    print("glob pattern:", glob_pattern)
+    audio_files = glob(glob_pattern, recursive=True)
 
-    output_path.parent.mkdir(parents=True, exist_ok=True)
-    np.save(output_path, ecmae_features.detach().cpu().numpy())
+    for audio_file in tqdm(audio_files):
+        try:
+            audio_file = Path(audio_file)
+
+            output_path = (
+                features_dir / model / audio_file.parent.name / audio_file.name
+            ).with_suffix(".npy")
+
+            if output_path.exists():
+                continue
+
+            xorig, _ = librosa.core.load(audio_file, sr=sr)
+
+            if model in ("base", "large"):
+                encoder.visible_encoder.compile = False
+                n_seconds = 4
+
+                n_patches = xorig.shape[0] // (n_seconds * fs)
+                xorig = xorig[: n_patches * n_seconds * fs].reshape(-1, n_seconds * fs)
+
+                x = {
+                    "wav": torch.from_numpy(xorig).to(device=device, dtype=ecmae.dtype),
+                    "wav_lens": torch.tensor(
+                        [
+                            xorig.shape[1],
+                        ]
+                        * xorig.shape[0],
+                        device=device,
+                    ),
+                }
+
+                with torch.no_grad():
+                    encoder.encode_wav(x)
+                    encoder.mask(x, ignore_mask=True)
+                    encoder.encode_visible(x)
+                    features = x["visible_embeddings"]
+
+            else:
+                features = encoder.encode(xorig)
+
+
+            output_path.parent.mkdir(parents=True, exist_ok=True)
+            np.save(output_path, features.detach().cpu().numpy())
+
+        except Exception:
+            print(f"`{audio_file}` failed")
+            
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument("audio_dir", type=Path)
+    parser.add_argument("features_dir", type=Path)
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="base",
+        choices=[
+            "base",
+            "large",
+            "diffusion_1s",
+            "diffusion_4s",
+            "diffusion_10s",
+        ],
+    )
+    parser.add_argument("--format", type=str, default=".wav", choices=[".wav", ".mp3"])
+    parser.add_argument("--device", type=str, default="cuda:0")
+
+    args = parser.parse_args()
+
+    model = args.model
+
+    encode_encodecmae(
+        args.audio_dir,
+        args.features_dir,
+        model,
+        device=args.device,
+        audio_format=args.format,
+    )
